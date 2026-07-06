8c66c032b0129d8a01e06958d945ac415216c540
Jonathan <jonthan0310@gmail.com>
2026-07-06 12:22:39 -0600

    chore: cambiar puerto default a 8080 y agregar run.ps1

--- config/settings/local.py
+++ config/settings/local.py
@@ -16,8 +16,11 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
+    'http://localhost:8080',
     'http://127.0.0.1:8000',
+    'http://127.0.0.1:8080',
     'http://10.10.0.198:8000',
+    'http://10.10.0.198:8080',
     'https://*.ngrok-free.dev',
     'https://*.ngrok.io',
 ]
